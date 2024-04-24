6f31aec2ae16fb08f649602616ea4c4fe7bfd14e
Julian-88 <144748312+Julian-88@users.noreply.github.com>
2024-04-24 18:12:03 +0000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,12 @@ for (let i = 1; i <= 10; i++) {
     let squared = i * i; 
     console.log(`The square of ${i} is ${squared}`); 
   } 
+
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+
+   if (i === 0) {
+       console.log("Blast Off!");
+   }
+}
