f1a7e57efbe4673c633e2fe5f43065c06016b319
Julian-88 <144748312+Julian-88@users.noreply.github.com>
2024-06-07 19:11:15 +0000

    ee

--- scripts/new.js
+++ scripts/new.js
@@ -18,3 +18,11 @@ document.getElementById('cta-button').addEventListener('click', function(event) 
     loadPage('features');
 });
 
+const images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'];
+let currentImage = 0;
+const galleryImage = document.getElementById('galleryImage');
+
+setInterval(() => {
+    galleryImage.src = images[currentImage];
+    currentImage = (currentImage + 1) % images.length;
+},2000);
