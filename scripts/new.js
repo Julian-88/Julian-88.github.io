f12c5fbb4ebb16d15aba293b321d1d3d0c88f6bc
Julian-88 <144748312+Julian-88@users.noreply.github.com>
2024-06-11 19:03:58 +0000

    fixed website back before expirements

--- scripts/new.js
+++ scripts/new.js
@@ -1,23 +1,3 @@
-document.getElementById('home.html').addEventListener('click', function(event) {
-    event.preventDefault();
-    loadPage('home');
-});
-
-document.getElementById('features.html').addEventListener('click', function(event) {
-    event.preventDefault();
-    loadPage('features');
-});
-
-document.getElementById('contact.html').addEventListener('click', function(event) {
-    event.preventDefault();
-    loadPage('contact');
-});
-
-document.getElementById('cta-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    loadPage('features');
-});
-
 const images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg',];
 let currentImage = 0;
 const galleryImage = document.getElementById('galleryImage');
